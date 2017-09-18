23f95418e8f333b814b4ba1bd6131abfe0bd154a
Chris Nady <nady.christopher@gmail.com>
2017-09-18 20:47:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function doToElementsInArray(array, callback) {
 function changeCompletely(element, index, array) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
+
+function iterativeLog(array) {
+
+}
+
+function iterate(callback) {
+
+}
+
+function doToArray(array, callback){
+  
+}
