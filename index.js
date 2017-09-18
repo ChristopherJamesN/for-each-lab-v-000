7538415d3e62a4612c4fecf1f3a09ebc1c678ab1
Chris Nady <nady.christopher@gmail.com>
2017-09-18 21:06:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function iterativeLog(array) {
-  array.forEach((element, index) => {console.log('${index}: ${element}')})
-}
+  array.forEach((element, index) => {
+    console.log('${index}: ${element}')
+  }
+)}
 
 function iterate(callback) {
   array = [1,2,3]
